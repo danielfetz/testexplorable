d687d5fe840fa50e77b35560043fbad3b9233701
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-09-08 21:36:57 +0200

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SwitzerlandWWIIExplanation from './SwitzerlandWWIIExplanation';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SwitzerlandWWIIExplanation />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
